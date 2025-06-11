67bf48ec533e28f568139f84c7b29a0b80828083
tdrive <tdrive@xc0de.ru>
2025-06-11 11:27:26 +0300

    Layout/TrailingWhitespace

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem "rackup"
 gem "puma"
 gem "sqlite3"
 
-# irb/input-method.rb:278: warning: rdoc was loaded from the standard library, 
+# irb/input-method.rb:278: warning: rdoc was loaded from the standard library,
 # but will no longer be part of the default gems starting from Ruby 3.5.0.
 # You can add rdoc to your Gemfile or gemspec to silence this warning.
 gem "rdoc"
--- db/migrations/1_schema.rb
+++ db/migrations/1_schema.rb
@@ -6,28 +6,28 @@ Sequel.migration do
       String :name, size: 255, null: false
       String :type, size: 255
       String :value, size: 255
-      
+
       index [:id], name: :table_name_id_uindex, unique: true
     end
-    
+
     create_table(:templates, ignore_index_errors: true) do
       primary_key :id
       String :name, size: 255, null: false
       Integer :discount, null: false
       Integer :cashback, null: false
-      
+
       index [:id], name: :template_id_uindex, unique: true
     end
-    
+
     create_table(:users, ignore_index_errors: true) do
       primary_key :id
       foreign_key :template_id, :templates, null: false
       String :name, size: 255, null: false
       BigDecimal :bonus
-      
+
       index [:id], name: :user_id_uindex, unique: true
     end
-    
+
     create_table(:operations, ignore_index_errors: true) do
       primary_key :id
       foreign_key :user_id, :users, null: false
@@ -39,7 +39,7 @@ Sequel.migration do
       BigDecimal :check_summ, null: false
       TrueClass :done
       BigDecimal :allowed_write_off
-      
+
       index [:id], name: :operation_id_uindex, unique: true
     end
   end
